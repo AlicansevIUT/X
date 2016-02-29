dd509376881ceda67855c0f12f109c869667ecf7
canseva <canseva@PC-A102-06.iut-valence.fr>
2016-02-29 16:30:17 +0100

    Initialisation

--- src/ChineseCheckersGame.java
+++ src/ChineseCheckersGame.java
@@ -1,10 +1,29 @@
+/**
+ * A Chinese checkers game
+ * (http://www.dameschinoises.fr/regles-du-jeu/)
+ * @author canseva, berarddy
+ */
 
 public class ChineseCheckersGame
 {
-
+	
+	/**
+	 * Creates a new Chinese checkers game, ready to be played (board initialized, players are ready)
+	 */
+	public ChineseCheckersGame()
+	{
+		this.board = new Board();
+		
+	}
+	
+	/**
+	 * Explains the progress of the game
+	 */
 	public void play()
 	{
-		
+	
 	
 	}
+	
+	
 }
