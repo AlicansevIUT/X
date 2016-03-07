69239bcfe2d8c34ab3add618c16febc261feda4b
sebastienjean <baz.jean@gmail.com>
2016-03-07 10:35:29 +0100

    Renamed project, added some TODO

--- src/ChineseCheckersGame.java
+++ src/ChineseCheckersGame.java
@@ -1,3 +1,4 @@
+// TODO find a URL that describe this game in english
 /**
  * A Chinese checkers game
  * (http://www.dameschinoises.fr/regles-du-jeu/)
@@ -7,6 +8,7 @@
 public class ChineseCheckersGame
 {
 	
+	// TODO explain more accurately what "board initialized" mean
 	/**
 	 * Creates a new Chinese checkers game, ready to be played (board initialized, players are ready)
 	 */
@@ -15,6 +17,8 @@ public class ChineseCheckersGame
 		this.board = new Board();
 		
 	}
+	
+	// TODO rewrite this comment (what is the purpose of this method, what is the main algorithm)
 	
 	/**
 	 * Explains the progress of the game
--- src/main.java
+++ src/main.java
@@ -1,7 +1,9 @@
+// TODO update README.md so that it give relevant information about the project (what, who)
 
+// TODO rename this class (this is not compliant with coding conventions)
 public class main
 {
-
+	// TODO write Javadoc comment
 	public static void main(String[] args)
 	{
 		new ChineseCheckersGame().play();
